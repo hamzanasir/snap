04e9bdb147f8d9870d8ac67e62ca945cb46f5564
Gustavo Garcia <gustavogb@gmail.com>
2015-05-14 12:26:39 -0700

    Add index.js file

---
+++ index.js
@@ -0,0 +1,8 @@
+exports = {
+  Logging: require('./lib/logging.js'),
+  Stats: require('./lib/stats.js'),
+  HttpClient: require('./lib/http-client.js'),
+  HttpServer: require('./lib/http-server.js')
+};
+
+
