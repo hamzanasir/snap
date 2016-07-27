fd20d308171eb1e8ee0e0cb0d20a1321b4642b08
Gustavo Garcia <gustavogb@gmail.com>
2016-07-26 18:29:11 -0700

    ADD support for blacklisting urls in the http server filter

--- test/http-server-test.js
+++ test/http-server-test.js
@@ -5,30 +5,44 @@ var assert = require('assert'),
 
 describe('Server Filter', function() {
   it('should measure stats', function(done) {
-	function on(event, callback) {
-	  callback();
-	}
+    function on(event, callback) {
+      callback();
+    }
     var filter = http.filter({ name: 'server', logging: false });
     filter({}, { on: on }, function() {
 	    assert.equal(stats.stats()['server'].count, 1);
       done();
     });
   });
-  
+
   it('should generate logs for requests', function(done) {
     var logged = false;
     logging.getLogger('server1').info = function() {
       logged = true;
     };
-	function on(event, callback) {
-	  callback();
-	}
+    function on(event, callback) {
+      callback();
+    }
     var filter = http.filter('server1');
     filter({}, { on: on }, function() {
 	    assert(logged);
       done();
     });
   });
-  
+
+  it('shouldnt generate logs for blacklisted requests', function(done) {
+    var logged = false;
+    logging.getLogger('server1').info = function() {
+      logged = true;
+    };
+    function on(event, callback) {
+      callback();
+    }
+    var filter = http.filter({ name: 'server1', blacklist: /\/health/i });
+    filter({ url: '/health' }, { on: on }, function() {
+      assert(!logged);
+      done();
+    });
+  });
 });
 
